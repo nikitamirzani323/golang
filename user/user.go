24d003aa70ea7bd79cc52bf2b46973523fd80d05
nikitamirzani323 <nikitamirzani323@gmail.com>
2021-06-30 03:09:51 +0700

    USER

---
+++ user/user.go
@@ -0,0 +1,29 @@
+package user
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var err error
+
+const DNS = "root:admin@tcp(127.0.0.1:3306)/godb?charset=utf8mb4&parseTime=True&loc=Local"
+
+type User struct {
+	gorm.Model
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+}
+
+func InitialMigration() {
+	DB, err = gorm.Open(mysql.Open(DNS), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Cannot connect to Database")
+	}
+	DB.AutoMigrate(&User{})
+}
